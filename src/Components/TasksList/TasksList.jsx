2f08b54e45ef4b53eb5441d420447d08f61a2bf3
marcus-t430 <marcus.pinheiro@super.ufam.edu.br>
2026-05-11 17:31:35 -0400

    feat(TasksList): cria componente TasksList

---
+++ src/Components/TasksList/TasksList.jsx
@@ -0,0 +1,19 @@
+const TasksList = () => {
+  return (
+    <ul>
+      <li>
+        Item 1
+        <button>Delete</button>
+      </li>
+      <li>
+        Item 2
+        <button>Delete</button>
+      </li>
+      <li>
+        Item 3
+        <button>Delete</button>
+      </li>
+    </ul>
+  );
+};
+export { TasksList };
