58bc21533725dbd6700b43f470d70d685806c67f
marcus-t430 <marcus.pinheiro@super.ufam.edu.br>
2026-05-12 19:32:05 -0400

    refactor(TasksList): altera retorno do componente e adicione array de tasks

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -2,11 +2,26 @@ import style from "./TasksList.module.css";
 import { Item } from "../../components";
 
 const TasksList = () => {
+  const taskArray = [
+    {
+      id: 1,
+      name: "Item 1",
+    },
+    {
+      id: 2,
+      name: "Item 2",
+    },
+    {
+      id: 3,
+      name: "Item 3",
+    },
+  ];
+
   return (
     <ul className={style.TasksList}>
-      <Item task="Item 1"></Item>
-      <Item task="Item 2"></Item>
-      <Item task="Item 3"></Item>
+      {taskArray.map((item) => (
+        <Item key={item.id} task={item.name} />
+      ))}
     </ul>
   );
 };
