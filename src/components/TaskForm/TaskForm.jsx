c6f58bb7366537e5a6452040a44b3999df8dccf9
Desktop Marcus <marcus.pinheiro@super.ufam.edu.br>
2026-05-13 10:25:55 -0400

    feat(TaskForm): implementa criação de tarefas via formulário

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -2,11 +2,33 @@ import { Input, Button } from "../../components";
 import floppyDiskIcon from "../../assets/—Pngtree—pixel art floppy diskette icon_23116937.png";
 import pencilIcon from "../../assets/—Pngtree—pixel art pencil icon design_8529404.png";
 import style from "./TaskForm.module.css";
+import { useState } from "react";
+import { postTask } from "../../service/TaskService";
 
 const TaskForm = () => {
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleCreateTask = async (event) => {
+    event.preventDefault();
+
+    const taskData = {
+      title: taskTitle,
+      completed: false,
+    };
+
+    try {
+      const response = await postTask(taskData);
+
+      console.log("Task Created: ", response.data);
+      setTaskTitle("");
+    } catch (error) {
+      console.error("Error creating task: ", error);
+    }
+  };
+
   return (
     <>
-      <div className={style.TaskForm}>
+      <form className={style.TaskForm} onSubmit={handleCreateTask}>
         <img
           className={`${style.FormIcon} ${style.LeftIcon}`}
           src={pencilIcon}
@@ -18,9 +40,14 @@ const TaskForm = () => {
           alt=""
         />
         <h1 className={style.Title}>To Do List</h1>
-        <Input type="text" placeholder="Task title"></Input>
-        <Button title="Add" variant="add" />
-      </div>
+        <Input
+          type="text"
+          placeholder="Task title"
+          value={taskTitle}
+          onChange={(event) => setTaskTitle(event.target.value)}
+        />
+        <Button title="Add" variant="add" type="submit" />
+      </form>
     </>
   );
 };
